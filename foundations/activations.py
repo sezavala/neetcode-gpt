74f4a83f9034212c26a2394beb48f47697a3ab4e
Sergio Zavala <106635253+sezavala@users.noreply.github.com>
2026-06-20 15:02:52 -0700

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -8,8 +8,8 @@ class Solution:
         # z is a 1D NumPy array
         # Formula: 1 / (1 + e^(-z))
         # return np.round(your_answer, 5)
-        arr = 1 / (1 + np.exp(-z))
-        return np.round(arr, 5)
+        z = 1 / (1 + math.e**(-z))
+        return np.round(z, 5)
 
     def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
